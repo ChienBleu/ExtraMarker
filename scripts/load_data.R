ae82f00f0a6b9b50860be77019956fa218fd9c63
Zacharie Menetrier <zacharie.menetrier@gmail.com>
2017-12-30 13:59:19 +0100

    Reduce data and log data

--- scripts/load_data.R
+++ scripts/load_data.R
@@ -17,6 +17,24 @@ geochar <- lapply(split(colData(rse_gene), seq_len(nrow(colData(rse_gene)))), re
 disease.types <- unlist(as.factor(unlist(lapply(geochar, "[", 4))))
 # Create the DESeq2 object
 rse <- scale_counts(rse_gene)
-colData(rse)$group <- disease.types
-dds <- DESeqDataSet(rse, ~ group)
-vsd <- vst(dds)
+count.table <- data.frame(t(assay(rse)))
+rm(rse)
+rm(geochar)
+rm(rse_gene)
+# Reducing the data set
+count.table$disease <- disease.types
+sCC <- count.table[count.table$disease == "Colorectal Cancer",]
+sPC <- count.table[count.table$disease == "Prostate Cancer",]
+sHC <- count.table[count.table$disease == "Healthy Control",]
+selected <- sCC[sample(nrow(sCC), 20), ]
+selected <- rbind(selected, sPC[sample(nrow(sPC), 20), ])
+selected <- rbind(selected, sHC[sample(nrow(sHC), 20), ])
+count.table <- as.matrix(selected[,-58038])
+sCC <- as.matrix(sCC[,-58038])
+sPC <- as.matrix(sPC[,-58038])
+sHC <- as.matrix(sHC[,-58038])
+disease.types <- selected$disease
+rm(selected)
+# Log the data
+epsilon <- 1
+log.count.table <- log2(count.table + epsilon)
