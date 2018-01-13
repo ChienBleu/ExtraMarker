b8151ef1d06b01b6dbc4073aac7645b663f7195c
Zacharie Menetrier <zacharie.menetrier@gmail.com>
2018-01-13 11:09:55 +0100

    Random feature selection

---
+++ scripts/random_selection.R
@@ -0,0 +1,36 @@
+library(MASS)
+library(caret)
+library(randomForest)
+
+n.samples <- length(disease.types)
+
+# Creation of the data frame
+all.data <- data.frame(t(log.count.table))
+
+# Test without feature selection
+mean.error.rates <- c()
+var.error.rates <- c()
+gene.number <- 2
+for(i in 1:12){
+  error <- c()
+  print(gene.number)
+  for(j in 1:20){
+    print(j)
+    # randomly select 10000 genes
+    reduce.data <- all.data[, sample(colnames(all.data), gene.number)]
+    reduce.data$class <- disease.types
+    # Partition of training and testing set
+    training.samples <- sample(seq_len(n.samples), size = n.samples * 0.66)
+    train <- reduce.data[training.samples, ]
+    test <- reduce.data[-training.samples, ]
+    
+    # Test with 15000 columns randomly selected
+    model <- randomForest(class ~ ., train)
+    predictions <- predict(model, test[,-ncol(test)])
+  
+    error <- c(error, sum(predictions != test$class)/ n.samples)
+  }
+  gene.number <- gene.number * 2
+  mean.error.rates <- c(mean.error.rates, mean(error))
+  var.error.rates <- c(var.error.rates, var(error))
+}
